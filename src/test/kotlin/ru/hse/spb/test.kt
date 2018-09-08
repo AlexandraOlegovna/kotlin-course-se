a606a42a58c11067748bc5ba2e82cd99759e917a
AlexandraOlegovna <nikolenko.rnd@gmail.com>
2018-09-08 03:40:49 +0300

    add tests

--- src/test/kotlin/ru/hse/spb/test.kt
+++ src/test/kotlin/ru/hse/spb/test.kt
@@ -5,7 +5,24 @@ import org.junit.Test
 
 class TestSource {
     @Test
-    fun testGreeting() {
-        assertEquals("Hello, world!", getGreeting())
+    fun testFromCodeforces1() {
+        assertEquals(3.729935587093555327,
+                solver(Point(0.0, 0.0),
+                        Point(5.0, 5.0),
+                        3.0,
+                        2.0,
+                        Point(-1.0, -1.0),
+                        Point(-1.0, 0.0)), 0.000001)
+    }
+
+    @Test
+    fun testFromCodeforces2() {
+        assertEquals(11.547005383792516398,
+                solver(Point(0.0, 0.0),
+                        Point(0.0, 1000.0),
+                        100.0,
+                        1000.0,
+                        Point(-50.0, 0.0),
+                        Point(50.0, 0.0)), 0.000001)
     }
 }
